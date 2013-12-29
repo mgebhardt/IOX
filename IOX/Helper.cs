a70bade9396e7d2c303d115f7aa88234db226434
Mathias Gebhardt (PC) <gebhardtmathias@aol.com>
2013-12-29 15:35:13 +0100

    Add file header

--- IOX/Helper.cs
+++ IOX/Helper.cs
@@ -1,7 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿/*
+ * Version      1.0.0
+ * Package      IO Exchanger
+ * Subpackage   Helper class
+ * Copyright    (c) 2013 Mathias Gebhardt
+ * License      GNU General Public License version 3 or later 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+/*============================================================================*/
+
+using System;
 
 /*============================================================================*/
 
